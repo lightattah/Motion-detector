26006cb40d4010add58c3f6e12dfa3265e5548ab
Light Attah <73713921+light101-cyber@users.noreply.github.com>
2021-12-29 15:00:13 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,17 +1,20 @@
 from motion_detector import df
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
 
-height = []
-base = []
+df["Entry_string"] = df["Entry"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["Exit_string"] = df["Exit"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+cds = ColumnDataSource(df)
+
 p = figure(x_axis_type='datetime', height = 100, width = 500, title="Motion Graph")
+p.yaxis.minor_tick_line_color = None
 
-for i in range(len(df["Entry Time"])):
-    height.append(1)
+hover = HoverTool(tooltips=[("Entry: ", "@Entry_string"),("Exit", "@Exit_string")])
+p.add_tools(hover)
 
-for item in height:
-    base.append(0)
-
-q = p.quad(top = height, bottom = base, left = df["Entry Time"], right = df["Exit Time"], color = "Orange")
+q = p.quad(top = 1, bottom = 0, left = "Entry", right = "Exit", color = "Orange", source=cds)
 
 output_file("TIme plot.html")
 show(p)
