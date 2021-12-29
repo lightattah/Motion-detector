e7759028e60146cabc61a4a8eb0f7f580b546ccb
Light Attah <73713921+light101-cyber@users.noreply.github.com>
2021-12-29 13:40:33 +0100

    Update motion_detector.py

--- motion_detector.py
+++ motion_detector.py
@@ -1,13 +1,22 @@
-import cv2, time
+import cv2
+import pandas
+from datetime import datetime
+
 first_frame = None
 video = cv2.VideoCapture(0)
+status = None
+status_list = [None, None]
+entry_times = []
+exit_times = []
+df = pandas.DataFrame(columns=["Entry Time", "Exit Time"])
 #The number 0 above can be changed to 1, 2 etc if there are multiple cameras connected to the PC. Changing the number would change the input device
 
 while True:
     check, frame = video.read()
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21,21), 0)
+    # Converting first frame to grayscale and applying Gaussian Blur improves accuracy of the program
 
     if first_frame is None:
         first_frame = gray
@@ -20,14 +29,21 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 5000:
             continue
-        #We want to ignore any motion less than 1000 pixels(100*10, 32*32 etc), as they are too small in this instance
-        #This number is purely based on taste, and could be different for a security camera, for example; as this has a much higher range and moving objects would be much smaller.  
-        
+        status = 1
         (x,y,w,h) = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3) #The colour could be changed by altering the values in the 3*1 BGR tuple. I chose blue here.
-        #draws bounding square/rectangle around moving object 
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 3)
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    # Capturing the entry and exit times
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        entry_times.append(datetime.now())
+
+    elif status_list[-1] == 0 and status_list[-2] == 1:
+        exit_times.append(datetime.now())
 
     cv2.imshow("Grey Frame", gray)
     cv2.imshow("Delta Frame", delta_frame)
@@ -38,5 +54,16 @@ while True:
     key = cv2.waitKey(1)
 
     if key == ord("q"):
-        break #The motion detection in-video process would stop when the key "q" is pressed on the keyboard. Can be changed to taste.
+        if status == 1:
+            exit_times.append(datetime.now())
+        break
+
+print(status_list)
+df["Entry Time"] = entry_times
+df["Exit Time"] = exit_times
+
+df.to_csv("Times.csv")
+
+print(df)
+
 video.release()
